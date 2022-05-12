7c71865e724da5d05c759dc54a6cf95176729ebe
Jose1i1o <guadix88@gmail.com>
2022-05-13 01:07:35 +0200

    feat(variables): Print variables inside a fragment

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -5,7 +5,13 @@
 // With hooks Functional Components are much easier to maintain and to read
 
 // This is a FC
-const FirstComponent = () => { 
+const FirstComponent = () => {
+
+  const person = { 
+    name: 'Jose',
+    age: 33
+  }
+  // To use variables we will use {} in the HTML syntax. We can use strings, number, arrays but not booleans. 
 
   // Return needs the parenthesis if we have a space, breakdown between return and the html or if there is more than one tag. All the code also needs to be contained within one tag/ element to keep the reference easily. For example a <div></div>.
 
@@ -14,8 +20,9 @@ const FirstComponent = () => {
   // See the html in the console to see the different inserted elements
   return (
   <>
-    <h1>Have a great day! ❤️</h1> 
-    <span>My name is Jose and I hope you are ok</span>
+    <h1>Have a great day! ❤️</h1>
+    {/* <pre> { JSON.stringify(person) } </pre> Text in the tag pre is shown in a fixed font, it´s not proportional, it is exactly show as it is in the file */}
+    <span>My name is { person.name } and I hope you are ok</span>
   </>
   )
 }
