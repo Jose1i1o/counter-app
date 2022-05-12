234300be42beb60e9a13b0089b52833435677b80
Jose1i1o <guadix88@gmail.com>
2022-05-13 00:25:25 +0200

    feat(firstComponent): add first Component with Comments

---
+++ src/FirstComponent.js
@@ -0,0 +1,12 @@
+// we have two kind of components: those based on classes and those based in components
+// The last are now called functional components, previously SFC (Stainless Functional Components)
+// With hooks Functional Components are much easier to maintain and to read
+
+// This is a FC
+const FirstComponent = () => { 
+
+  return <h1>Have a great day! ❤️</h1> 
+
+}
+
+export default FirstComponent;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-
-const hello = <h1>I love you</h1>
+import FirstComponent from "./FirstComponent";
+import './main.css'; // here we need to specify it is css as it won´t differentiate between the two
 
 
 const divRoot = document.getElementById("root");
 
 // document.body.append(hello) This doesn´t work but it would be the same
 
-createRoot(divRoot).render(hello)
+createRoot(divRoot).render(<FirstComponent />)
