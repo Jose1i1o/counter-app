7f263ef5a3ba17453a89098ea3e3fb6e9fd6b083
Jose1i1o <guadix88@gmail.com>
2022-05-30 10:15:43 +0200

    fix(enzyme tests commented): hide enzyme tests not functional on react 18

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import CounterApp from '../CounterApp.js';
+// import { shallow } from 'enzyme';
+
+
+
 describe('Testing my first react Component <CounterApp />', () => {
 
   // test the counter is 0 with react testing library
@@ -28,3 +32,70 @@ describe('Testing my first react Component <CounterApp />', () => {
     expect( getByText('1') ).toBeInTheDocument();
   })
 })
+
+
+
+// Testing with enzyme
+
+// describe('Testing <CounterApp />', () => {
+    
+//     let wrapper = shallow( <CounterApp /> );
+
+//     beforeEach( () =>  {
+
+//         wrapper = shallow( <CounterApp /> );
+
+//     });
+
+
+
+//     test('should display <CounterApp /> correctly', () => {
+       
+        
+//         expect( wrapper ).toMatchSnapshot();
+
+//     });
+
+
+//     // eslint-disable-next-line jest/valid-title
+//     test('should display the initial value as 100 ', () => {
+        
+//         const wrapper = shallow( <CounterApp value={ 100 } /> );
+
+//         const counterText = wrapper.find('h2').text().trim();
+        
+//         expect( counterText ).toBe('100');
+
+//     })
+
+//     test('should increase the counter by +1', () => {
+
+        
+//         wrapper.find('button').at(0).simulate('click');
+//         const counterText = wrapper.find('h2').text().trim();
+//         expect( counterText ).toBe('11');
+
+//     })
+
+//     test('should decrease the counter by -1', () => {
+        
+//         wrapper.find('button').at(2).simulate('click');
+//         const counterText = wrapper.find('h2').text().trim();
+//         expect( counterText ).toBe('9');
+
+//     });
+
+
+//     test('should set back to the default value', () => {
+//         const wrapper = shallow( <CounterApp value={ 105 } /> );
+
+//         wrapper.find('button').at(0).simulate('click');
+//         wrapper.find('button').at(0).simulate('click');
+//         wrapper.find('button').at(1).simulate('click');
+
+//         const counterText = wrapper.find('h2').text().trim();
+
+//         expect( counterText ).toBe('105');
+
+//     })
+// })
