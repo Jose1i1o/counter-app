4465c5343db9ed8a0beee7396d8ac89fed8a384c
Jose1i1o <guadix88@gmail.com>
2022-05-31 14:44:06 +0200

    feat(customhook): add custom hook to increase flexibility of components

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,28 +1,23 @@
-import { useState, useEffect } from "react";
-import getGifs from "../helpers/getGifs";
 import GifGridItem from "./GifGridItem";
+import useFetchGifs from "./hooks/useFetchGifs";
 
 const GifGrid = ({ category }) => {
-  
-  const [ images, setImages ] = useState([]);
 
-  useEffect(() => {
-    getGifs( category )
-      .then( setImages );
-  },[ category ])
+  const { data:images, loading } = useFetchGifs( category );
 
   return (
     <>
       <h3>{ category }</h3>
-      <div className="card-grid">
-        {
-         images.map(img  => (
-           <GifGridItem
-              key={ img.id }
-              { ...img }
-           />
-         ))
-        }
+      { loading && <p>Loading...</p>}
+      <div className="card-grid ">
+      {
+      images.map( img => (
+        <GifGridItem
+          key={ img.id }
+          {...img}
+        />
+        ))
+      }
       </div>
     </>
   )
---
+++ src/components/hooks/useFetchGifs.js
@@ -0,0 +1,30 @@
+// name it with use to indicate it is a hook
+import { useState, useEffect } from "react";
+import getGifs from "../../helpers/getGifs";
+
+const useFetchGifs = ( category ) => {
+
+    const [state, setState] = useState({
+      data: [],
+      loading: true
+    });
+
+    // effects can not be async because they are expecting something synchronous
+    useEffect(() => {
+      getGifs( category )
+        .then( images => {
+          setTimeout(() => {
+            setState({
+            data: images,
+            loading: false
+          })
+        }, 500)
+      })
+    }, [ category ])
+
+  return state;
+
+}
+
+
+export default useFetchGifs;
